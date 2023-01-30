adbd0cbcde7a2ff237c687e0c7ff65657493bfd1
marshalharman <marshalharman@gmail.com>
2023-01-30 16:12:03 +0530

    Added bean package

---
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/bean/Student.java
@@ -0,0 +1,7 @@
+package com.flipkart.bean;
+
+public class Student {
+
+    private String branch;
+    private boolean isApproved;
+}
---
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/bean/User.java
@@ -0,0 +1,7 @@
+package com.flipkart.bean;
+
+public class User {
+    private int userID;
+    private String name;
+    private String address;
+}
