130da3ff6e2dc176fe39d8b26a4f895500444627
chiragagrawal <chirag.agrawal1@flipkart.com>
2023-01-30 16:22:26 +0530

    added admin services

---
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/service/AdminService.java
@@ -0,0 +1,32 @@
+package com.flipkart.service;
+
+public class AdminService {
+
+    public void login() {
+
+    }
+
+    public void approveStudentRegistration() {
+
+    }
+
+    public void addProfessor() {
+
+    }
+
+    public void removeProfessor() {
+
+    }
+
+    public void addCourse() {
+
+    }
+
+    public void removeCourse() {
+
+    }
+
+    public void generateGradeCard() {
+        
+    }
+}
