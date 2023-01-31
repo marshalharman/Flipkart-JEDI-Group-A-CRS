0b6a468d1fc1d629b672d8c98ebfa93d93a5f462
ruthvik186830 <123439670+ruthvik186830@users.noreply.github.com>
2023-01-31 09:44:09 +0530

    updated student client file

---
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/client/StudentApplication.java
@@ -0,0 +1,19 @@
+package com.flipkart.client;
+
+public class StudentApplication {
+    private void addCourse(){
+
+    }
+    private void deleteCourse(){
+
+    }
+    private void viewCourses(){
+
+    }
+    private void viewGrades(){
+
+    }
+    private void payFees() {
+
+    }
+}
