aa1c62d2c53f9172c68c1175779b8b4c8df55a07
Priyanshu Mangal <mangalpriyanshu456@gmail.com>
2023-01-31 14:56:01 +0530

    studentmenuadded

--- CRS-JEDI-JAVA-Development-POS/src/com/flipkart/client/CRSStudentMenu.java
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/client/CRSStudentMenu.java
@@ -2,10 +2,11 @@ package com.flipkart.client;
 
 import com.flipkart.service.StudentServiceOperation;
 
+import java.util.Scanner;
+
 public class CRSStudentMenu {
 
     StudentServiceOperation service = new StudentServiceOperation();
-
     private void register(){
 
     }
@@ -24,5 +25,35 @@ public class CRSStudentMenu {
     }
     private void payFees() {
 
+    }
+    public void createMenu(int id){
+        System.out.println("menu is created");
+        System.out.println("Student Menu!\n");
+        System.out.println("Choose one of the options\n");
+        System.out.println("1. Course Registration\n");
+        System.out.println("2. Add Course\n");
+        System.out.println("3. Drop Course\n");
+        System.out.println("4. View Course\n");
+        System.out.println("5. View grades\n");
+        System.out.println("6. Make Payment\n");
+        Scanner obj = new Scanner(System.in);
+        String choice;
+        choice = obj.nextLine();
+        switch(choice) {
+            case "1":
+                register();
+            case "2":
+                addCourse();
+            case "3":
+                deleteCourse();
+            case "4":
+                viewCourses();
+            case "5":
+                viewGrades();
+            case "6":
+                payFees();
+            default:
+                System.out.println("Menu\n");
+        }
     }
 }
