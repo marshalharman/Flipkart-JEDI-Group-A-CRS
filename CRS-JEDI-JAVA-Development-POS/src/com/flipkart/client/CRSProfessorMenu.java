ce8c23bdd8676bcbaee769f37722518a78181b09
somilsingla <somilsingla12@gmail.com>
2023-01-31 16:18:45 +0530

    Changes in CRS Admin Menu

--- CRS-JEDI-JAVA-Development-POS/src/com/flipkart/client/CRSProfessorMenu.java
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/client/CRSProfessorMenu.java
@@ -8,30 +8,42 @@ public class CRSProfessorMenu {
     ProfessorServiceOperation service = new ProfessorServiceOperation();
     public void professorMenu()
     {
-        System.out.println("Professor Menu!");
-        System.out.println("Choose one of the options");
-        System.out.println("1. View Courses");
-        System.out.println("2. Give course preference");
-        //System.out.println("3. De-register from a course");
-        System.out.println("4. View enrolled students");
-        System.out.println("5. Add grade");
 
-        Scanner obj = new Scanner(System.in);
-        String choice;
-        choice = obj.nextLine();
-        switch(choice) {
-            case "1":
-                viewCourses();
-            case "2":
-                registerCourse();
-            case "3":
-                deRegisterCourse();
-            case "4":
-                viewEnrolledStudent();
-            case "5":
-                addGrades();
-            default:
-                System.out.println("Menu\n");
+        while(true) {
+
+            System.out.println("Professor Menu!");
+            System.out.println("Choose one of the options");
+            System.out.println("1. View Courses");
+            System.out.println("2. Give course preference");
+            System.out.println("3. View enrolled students");
+            System.out.println("4. Add grade");
+            System.out.println("5. Logout");
+
+            Scanner obj = new Scanner(System.in);
+            int choice = Integer.parseInt(obj.nextLine());
+
+            switch (choice) {
+                case 1:
+                    viewCourses();
+                    break;
+                case 2:
+                    registerCourse();
+                    break;
+                case 3:
+                    viewEnrolledStudent();
+                    break;
+                case 4:
+                    addGrades();
+                    break;
+                case 5:
+                    System.out.println("Logged Out");
+                    break;
+                default:
+                    System.out.println("Please enter a valid input\n");
+            }
+
+            if(choice == 5 ){break;}
+
         }
     }
 	private void viewCourses(){
