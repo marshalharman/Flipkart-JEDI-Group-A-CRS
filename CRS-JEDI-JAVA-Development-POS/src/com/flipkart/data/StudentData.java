2730451a83ec85d78caf9c8ef4d6855f464e120c
marshalharman <marshalharman@gmail.com>
2023-02-01 16:47:23 +0530

    Added dummy data package

---
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/data/StudentData.java
@@ -0,0 +1,17 @@
+package com.flipkart.data;
+
+public class StudentData {
+
+
+
+    // list of Students
+
+    // semID - courses
+
+    // courses - freq
+
+    // student - courses and grades
+
+
+
+}
