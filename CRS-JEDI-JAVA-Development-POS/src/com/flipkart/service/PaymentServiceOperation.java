17d495dc5173d6aebe6f5e23564290f155155b96
chiragagrawal <chirag.agrawal1@flipkart.com>
2023-02-02 11:42:00 +0530

    resolved conflict

--- CRS-JEDI-JAVA-Development-POS/src/com/flipkart/service/PaymentServiceOperation.java
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/service/PaymentServiceOperation.java
@@ -1,12 +1,28 @@
 package com.flipkart.service;
+import com.flipkart.bean.Student;
+import com.flipkart.data.Data;
 import javax.swing.plaf.synth.SynthTextAreaUI;
 import java.lang.Math;
+import java.util.Map;
 
 public class PaymentServiceOperation implements PaymentInterface {
 
     @Override
-    public void pay() {
+    public void pay(Student student) {
+        int s_id = student.getUserID();
 
+        int reg_courses_number = Data.registeredCourses.get(s_id).size();
+
+        int fee_amount = 1000*reg_courses_number;
+        System.out.println("Your fee amount = "+fee_amount);
+        System.out.println("Select a method to pay -");
+        System.out.println("1.UPI   2.Cheque");
+
+        System.out.println("Payment done");
+
+
+
+        sendNotification(s_id,6723154,"Payment succesfull");
     }
 
     @Override
