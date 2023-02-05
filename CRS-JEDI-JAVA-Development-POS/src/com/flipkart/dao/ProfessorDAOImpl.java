50b51307ca6d64963ffcba8e38a243ca1f84c3f7
Priyanshu Mangal <mangalpriyanshu456@gmail.com>
2023-02-05 23:13:13 +0530

    updated

--- CRS-JEDI-JAVA-Development-POS/src/com/flipkart/dao/ProfessorDAOImpl.java
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/dao/ProfessorDAOImpl.java
@@ -5,12 +5,17 @@ import com.flipkart.bean.Professor;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.*;
+//import java.sql.PreparedStatement;
+//import java.sql.Connection;
+//import java.sql.SQLException;
 
 public class ProfessorDAOImpl implements ProferssorDAO{
 
     @Override
     public List<Course> getCoursesByProfessor(String userId) {
         List<Course> courseList=new ArrayList<Course>();
+        //PreparedStatement statement=Connection.prepareStatement("select * from course where professorId=?");
         return courseList;
     }
 
