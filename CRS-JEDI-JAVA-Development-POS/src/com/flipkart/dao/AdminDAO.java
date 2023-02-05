6decf49b1a3efc470252f491280bb46ff2671e8e
Priyanshu Mangal <mangalpriyanshu456@gmail.com>
2023-02-06 00:13:16 +0530

    admin dao added

--- CRS-JEDI-JAVA-Development-POS/src/com/flipkart/dao/AdminDAO.java
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/dao/AdminDAO.java
@@ -1,4 +1,29 @@
 package com.flipkart.dao;
+import java.sql.SQLException;
+import java.util.List;
 
+import com.flipkart.bean.Course;
+import com.flipkart.bean.Professor;
+import com.flipkart.bean.Student;
+import com.flipkart.bean.User;
 public interface AdminDAO {
+    public void deleteCourse(String courseCode) throws SQLException;// throws CourseNotFoundException, CourseNotDeletedException;
+
+    public void addCourse(Course course) throws SQLException;// throws CourseFoundException;
+
+//    public List<Student> viewPendingAdmissions();
+
+    public void approveStudent(int studentId) throws SQLException;//throws StudentNotFoundForApprovalException;
+
+
+    public void addProfessor(Professor professor) throws SQLException;// throws ProfessorNotAddedException, UserIdAlreadyInUseException;
+
+    public void addUser(User user) throws SQLException;// throws UserNotAddedException, UserIdAlreadyInUseException;
+
+
+//    public void assignCourse(String courseCode, String professorId);// throws CourseNotFoundException, UserNotFoundException;
+//
+//    public List<Course> viewCourses(int catalogId);
+//
+//    public List<Professor> viewProfessors();
 }
