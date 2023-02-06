5abc610114f30aa1d2d0ad5e377d1fa74dd0fe29
Priyanshu Mangal <mangalpriyanshu456@gmail.com>
2023-02-06 16:58:58 +0530

    Admin dao and service function updated

--- CRS-JEDI-JAVA-Development-POS/src/com/flipkart/dao/AdminDAO.java
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/dao/AdminDAO.java
@@ -7,18 +7,18 @@ import com.flipkart.bean.Professor;
 import com.flipkart.bean.Student;
 import com.flipkart.bean.User;
 public interface AdminDAO {
-    public void deleteCourse(String courseCode) throws SQLException;// throws CourseNotFoundException, CourseNotDeletedException;
+    public void deleteCourse(Integer courseCode) throws SQLException, ClassNotFoundException;// throws CourseNotFoundException, CourseNotDeletedException;
 
-    public void addCourse(Course course) throws SQLException;// throws CourseFoundException;
+    public void addCourse(Course course) throws SQLException, ClassNotFoundException;// throws CourseFoundException;
 
 //    public List<Student> viewPendingAdmissions();
 
-    public void approveStudent(int studentId) throws SQLException;//throws StudentNotFoundForApprovalException;
+    public void approveStudent(int studentId) throws SQLException, ClassNotFoundException;//throws StudentNotFoundForApprovalException;
 
 
-    public void addProfessor(Professor professor) throws SQLException;// throws ProfessorNotAddedException, UserIdAlreadyInUseException;
+    public void addProfessor(Professor professor) throws SQLException, ClassNotFoundException;// throws ProfessorNotAddedException, UserIdAlreadyInUseException;
 
-    public void addUser(User user) throws SQLException;// throws UserNotAddedException, UserIdAlreadyInUseException;
+    public void addUser(User user) throws SQLException, ClassNotFoundException;// throws UserNotAddedException, UserIdAlreadyInUseException;
 
 
 //    public void assignCourse(String courseCode, String professorId);// throws CourseNotFoundException, UserNotFoundException;
