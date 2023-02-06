efee759754cd1705150b022f6ef673be24476ce8
somilsingla <somilsingla12@gmail.com>
2023-02-06 15:15:23 +0530

    Implemented Professor DAO

--- CRS-JEDI-JAVA-Development-POS/src/com/flipkart/dao/ProferssorDAO.java
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/dao/ProferssorDAO.java
@@ -1,8 +1,16 @@
 package com.flipkart.dao;
 import com.flipkart.bean.Course;
+import com.flipkart.bean.Professor;
+
 import java.util.*;
 public interface ProferssorDAO {
-    public List<Course> getCoursesByProfessor(String userId);
+
+    public boolean registerCourseForProfessor(int profID, String courseName, int semID);
+    public List<Course> getCoursesByProfessor(int profID);
     public Boolean addGrade(int studentId,String courseCode,String grade);
-    public String getProfessorById(String profId);
+    public Professor getProfessorById(int profID);
+
+    public int getSemIDbyCourseID(int courseID);
+    public Course getCourseByName(String courseName);
+
 }
