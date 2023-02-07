7360c7aad022fc0893c56e687ea2dd4329718e35
sankalamaddy.r <sankalamaddy.r@flipkart.com>
2023-02-07 15:48:51 +0530

    updated class description

--- CRS-JEDI-JAVA-Development-POS/src/com/flipkart/dao/UserDAO.java
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/dao/UserDAO.java
@@ -1,15 +1,43 @@
 package com.flipkart.dao;
-
 import com.flipkart.bean.User;
 import com.flipkart.exception.UserNotFoundException;
 
+/**
+ *
+ * Interface for User Dao Operations
+ *
+ */
 public interface UserDAO {
 
-    // returns UserID if successful
+    /**
+     * Method to login using credentials
+     * @param userID
+     * @param password
+     * @param role
+     * @return boolean indicating if login is successful
+     * @throws UserNotFoundException
+     */
     public boolean login(int userID, String password, String role) throws UserNotFoundException;
-
+    /**
+     * Method to register a user
+     * @param userID
+     * @param userName
+     * @param password
+     * @param role
+     * @param isApproved
+     */
     public void register(int userID,String userName, String password, String role, boolean isApproved);
+    /**
+     * Method to update password of a user
+     * @param userID
+     * @param password
+     */
     public void updatePassword(int userID, String password);
+    /**
+     * Method to get details of user by id
+     * @param userID
+     * @return user object with all the details
+     */
     public User getUserByID(int userID);
 
     }
