9f508d19fec51516a3e0ab847eb8acfb38e6a749
sankalamaddy.r <sankalamaddy.r@flipkart.com>
2023-02-07 15:30:15 +0530

    updated class description

--- CRS-JEDI-JAVA-Development-POS/src/com/flipkart/dao/ProferssorDAO.java
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/dao/ProferssorDAO.java
@@ -2,22 +2,78 @@ package com.flipkart.dao;
 import com.flipkart.bean.Course;
 import com.flipkart.bean.Professor;
 import com.flipkart.bean.Student;
-
 import java.util.*;
+
+/**
+ *
+ * Interface for Professor Dao Operations
+ *
+ */
 public interface ProferssorDAO {
-
+    /**
+     * Method to view courses in a sem using sql command
+     * @param semID
+     * @return list of courses in a semester
+     */
     public List<Course> viewCoursesBySemID(int semID);
+    /**
+     * Method to register a professor to a course
+     * @param profID
+     * @param courseName
+     * @param semID
+     * @return a boolean indicating if professor is registered from a course
+     */
     public boolean registerCourseForProfessor(int profID, String courseName, int semID);
+    /**
+     * Method to de- register a professor to a course
+     * @param profID
+     * @param courseName
+     * @return a boolean indicating if professor is de-registered from a course
+     */
     public boolean deregisterCourseForProfessor(int profID, String courseName);
+    /**
+     * Method to view enrolled students in a course
+     * @param courseName
+     * @return list of enrolled students in a course
+     */
     public List<Student> viewEnrolledStudents( String courseName);
+    /**
+     * Method to get student details with id
+     * @param studentID
+     * @return student object with all details
+     */
     public Student getStudentByID(int studentID);
+    /**
+     * Method to list of courses taught by a professor
+     * @param profID
+     * @return list of courses taught by a professor
+     */
     public List<Course> getCoursesByProfessor(int profID);
+    /**
+     * Method to assign grade to students ina course
+     * @param studentId
+     * @param courseName
+     * @param grade
+     * @return boolean indicating if grades are assigned to the students
+     */
     public Boolean addGrade(int studentId, String courseName, String grade);
+    /**
+     * Method to get in which sem the course is being offered
+     * @param courseID
+     * @return sem number in which the course is being offered
+     */
     public int getSemIDbyCourseID(int courseID);
+    /**
+     * Method to get course details by name
+     * @param courseName
+     * @return course object with all the details
+     */
     public Course getCourseByName(String courseName);
+    /**
+     * Method to get professor details by id
+     * @param profID
+     * @return professor object with all details
+     */
     public Professor getProfessorById(int profID);
-
-
-
 
 }
