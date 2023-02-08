9ad9bb0c7e6a29077127462e58d5590fc430752b
chiragagrawal <chirag.agrawal1@flipkart.com>
2023-02-08 10:42:45 +0530

    fixed user already present exception

--- CRS-JEDI-JAVA-Development-POS/src/com/flipkart/dao/StudentDAOImpl.java
+++ CRS-JEDI-JAVA-Development-POS/src/com/flipkart/dao/StudentDAOImpl.java
@@ -2,9 +2,11 @@ package com.flipkart.dao;
 
 import com.flipkart.bean.Course;
 import com.flipkart.bean.Student;
+import com.flipkart.bean.User;
 import com.flipkart.constant.Dao;
 import com.flipkart.constant.Role;
 import com.flipkart.exception.DuplicateUserException;
+import com.flipkart.exception.UserIdAlreadyInUseException;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -15,11 +17,13 @@ import static com.flipkart.constant.Dao.*;
 
 public class StudentDAOImpl implements StudentDAO{
 
+    UserDAO userDAO = new UserDAOImpl();
+
     public void register(int studentID, String name, String address, String username, String password, String branch, String degree) throws DuplicateUserException {
         Connection conn = null;
         PreparedStatement stmt = null;
-        Student s=getStudentByID(studentID);
-        if(s!=null)
+        User u=userDAO.getUserByID(studentID);
+        if(u!=null)
         {
             throw new DuplicateUserException(studentID);
         }
